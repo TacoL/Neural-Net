f9ed7acf683258d234e0eff266b916ee4b466515
TacoL <34144367+TacoL@users.noreply.github.com>
2021-08-23 01:35:41 -0400

    Working Neural Network
    Tested on XOR - Works!!
    Still need to implement momentum scalar

---
+++ NeuralNet/NeuralNet/Neuron.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNet
+{
+    public class Neuron
+    {
+        public double neuronValue, activationValue, bias, neuronGradient, biasGradient;
+        public double[] weights, weightGradients;
+
+        public Neuron(int numInputs)
+        {
+            neuronValue = 0;
+            activationValue = 0;
+            neuronGradient = 0;
+
+            weights = new double[numInputs];
+            weightGradients = new double[numInputs];
+            weights.ToList().ForEach(w => w = Network.r.NextDouble());
+            bias = Network.r.NextDouble();
+            biasGradient = 0;
+        }
+
+        public void calcActivationValue(List<Neuron> inputNeurons)
+        {
+            neuronValue = 0; //reset neuron value every time
+
+            for (int i = 0; i < inputNeurons.Count; i++)
+                neuronValue += inputNeurons[i].activationValue * weights[i];
+            neuronValue += bias;
+
+
+            activationValue = activationFunction(neuronValue);
+        }
+
+        public double activationFunction(double x)
+        {
+            return Math.Tanh(x);
+        }
+
+        public double derivativeActivation(double x)
+        {
+            return 1 - Math.Pow(Math.Tanh(x), 2);
+        }
+        public void firstLayerSetup(double actV)
+        {
+            activationValue = actV;
+        }
+
+        public void updateWeightsAndBias()
+        {
+            //update weights and biases
+            for (int weightIdx = 0; weightIdx < weights.Length; weightIdx++)
+                weights[weightIdx] -= (weightGradients[weightIdx] / Network.batchSize) * Network.learningRate;
+            bias -= biasGradient / Network.batchSize;
+            //reset neuron and weight and bias GRADIENTS after each update (update every batch)
+            neuronGradient = 0;
+            weightGradients.ToList().ForEach(wg => wg = 0);
+            biasGradient = 0;
+        }
+    }
+}
---
+++ NeuralNet/NeuralNet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNet
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            //set up the network
+            Network.learningRate = 0.01;
+            Network.batchSize = 4;
+            Network nn = new Network(new int[] { 2, 10, 1 });
+
+            //set up training samples
+            List<TrainingSample> trainingSamples = new List<TrainingSample>();
+            trainingSamples.Add(new TrainingSample(new double[] {0, 0}, new double[] { 0 }));
+            trainingSamples.Add(new TrainingSample(new double[] {0, 1}, new double[] { 1 }));
+            trainingSamples.Add(new TrainingSample(new double[] {1, 0}, new double[] { 1 }));
+            trainingSamples.Add(new TrainingSample(new double[] {1, 1}, new double[] { 0 }));
+
+            //train network
+            for (int epoch = 0; epoch < 100000; epoch++)
+            {
+                double mse = 0;
+                trainingSamples.ForEach(sample => mse += nn.backPropagate(sample.inputs, sample.targets));
+                nn.updateWeightsAndBiases();
+
+                Console.WriteLine("Epoch: {0}         MSE: {1}", epoch + 1, mse / Network.batchSize);
+            }
+
+            //results
+            Console.WriteLine(nn.forwardPropagate(new double[] {0, 0})[0]);
+            Console.WriteLine(nn.forwardPropagate(new double[] {0, 1})[0]);
+            Console.WriteLine(nn.forwardPropagate(new double[] {1, 0})[0]);
+            Console.WriteLine(nn.forwardPropagate(new double[] {1, 1})[0]);
+        }
+    }
+}
